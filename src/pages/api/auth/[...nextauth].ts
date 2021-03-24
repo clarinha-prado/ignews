54eafb66c08f48f8394314078d68a2e3bb5d44f1
Clarinha Prado <clarinha.prado@gmail.com>
2021-03-24 18:19:13 -0300

    correção do erro da chave JWT

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,9 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
 
+import { query as q } from 'faunadb'
+import { fauna } from '../../../services/fauna'
+
 export default NextAuth({
     // Configure one or more authentication providers
     providers: [
@@ -10,4 +13,24 @@ export default NextAuth({
             scope: 'read:user'
         }),
     ],
+
+    callbacks: {
+        async signIn(user, account, profile) {
+            const { email } = user;
+
+            try {
+                await fauna.query(
+                    q.Create(
+                        q.Collection('users'),
+                        {
+                            data: { email }
+                        }
+                    )
+                );
+                return true;
+            } catch {
+                return false;
+            }
+        }
+    }
 })
