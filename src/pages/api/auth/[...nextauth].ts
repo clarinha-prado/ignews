8c05368149bd4ff0edf5ad264702e4a6ecbdb12f
Clarinha Prado <clarinha.prado@gmail.com>
2021-03-28 08:35:08 -0300

    integração com prismic

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import NextAuth from 'next-auth'
+import NextAuth, { Session } from 'next-auth'
 import Providers from 'next-auth/providers'
 
 import { query as q } from 'faunadb'
@@ -15,6 +15,45 @@ export default NextAuth({
     ],
 
     callbacks: {
+
+        async session(session: Session) {
+            try {
+                const userActiveSubscription = fauna.query(
+                    q.Get(
+                        q.Intersection([
+                            q.Match(
+                                q.Index('subscription_by_user_ref'),
+                                q.Select(
+                                    "ref",
+                                    q.Get(
+                                        q.Match(
+                                            q.Index('user_by_email'),
+                                            q.Casefold(session.user.email)
+                                        )
+                                    )
+                                )
+                            ),
+                            q.Match(
+                                q.Index('subscription_by_status'),
+                                "active"
+                            )
+                        ])
+                    ))
+
+                return {
+                    ...session,
+                    activeSubscription: userActiveSubscription
+                }
+            } catch (err) {
+                console.log(err.message);
+                return {
+                    ...session,
+                    activeSubscription: null
+                };
+            }
+
+        },
+
         // sign in só é feito com sucesso se essa função retornar true
         async signIn(user, account, profile) {
             const { email } = user;
